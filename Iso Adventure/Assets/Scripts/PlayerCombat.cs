9df1b22515788087486739a8ed2c13c0516a4202
Arilyn <34004269+arilynart@users.noreply.github.com>
2021-09-29 20:18:59 -0600

    Revert "resolving merge conflicts"
    This reverts commit af3db4496262b0ca4213acee535f687e8b71fa62.

--- Iso Adventure/Assets/Scripts/PlayerCombat.cs
+++ Iso Adventure/Assets/Scripts/PlayerCombat.cs
@@ -6,12 +6,8 @@ public class PlayerCombat : MonoBehaviour
 {
     PlayerController controller;
 
-    public Animator animator;
     public Transform basicAttackPoint;
-    public float basicAttackRange = 0.77f;
-    public int noOfPresses = 0;
-    System.DateTime lastPressedTime;
-    public float maxComboDelay = 0.43f;
+    public float basicAttackRange = 0.5f;
     public LayerMask enemyLayers;
 
     private void Start()
@@ -19,22 +15,14 @@ public class PlayerCombat : MonoBehaviour
         controller = GetComponent<PlayerController>();
     }
 
-    private void FixedUpdate()
-    {
-        if(System.DateTime.Now.Subtract(lastPressedTime).TotalSeconds > maxComboDelay)
-        {
-            noOfPresses = 0;
-            animator.SetBool("Basic Attack", false);
-            animator.SetBool("Basic Attack 2", false);
-        }
-
-    }
-
     public void BasicAttack()
     {
         //if not already dodging or falling (based on collision with floor)
         if (!controller.dodge && controller.collision == true)
         {
+            Debug.Log("You attack!");
+            //animation controls go here
+
             //detect enemies in range of attack
             Collider[] hitEnemies = Physics.OverlapSphere(basicAttackPoint.position, basicAttackRange, enemyLayers);
 
@@ -43,33 +31,6 @@ public class PlayerCombat : MonoBehaviour
             {
                 Debug.Log("Hit " + enemy.name);
             }
-
-            //animation controls go here
-            lastPressedTime = System.DateTime.Now;
-            noOfPresses++;
-            if (noOfPresses == 1)
-            {
-                animator.SetBool("Basic Attack", true);
-                Debug.Log("You attack once!");
-            }
-            else
-            {
-                animator.SetBool("Basic Attack", false);
-            }
-
-            if (noOfPresses == 2)
-            {
-                animator.SetBool("Basic Attack", false);
-                animator.SetBool("Basic Attack 2", true);
-                Debug.Log("You attack twice!");
-                noOfPresses = 0;
-            }
-            else
-            {
-                animator.SetBool("Basic Attack 2", false);
-            }
-            //noOfPresses = Mathf.Clamp(noOfPresses, 0, 2);
-            return;
         }
     }
 

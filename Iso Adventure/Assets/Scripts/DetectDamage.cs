9fd583ff4fe0126aa32866d728f4277716a62547
Aren <upinthed@aim.com>
2021-10-02 02:15:51 -0400

    Working Wander to Chase and Back States

--- Iso Adventure/Assets/Scripts/DetectDamage.cs
+++ Iso Adventure/Assets/Scripts/DetectDamage.cs
@@ -6,9 +6,13 @@ public class DetectDamage : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (!other) return;
+        if (!GetComponent<EnemyStats>()) return;
+        if (!transform.parent.gameObject.GetComponent<PlayerController>()) return;
+
         if (other.tag == "Attack" && !transform.parent.gameObject.GetComponent<PlayerController>().invuln)
         {
+            
             Debug.Log("Damage: " + other.transform.parent.GetComponent<EnemyStats>().activeAttack.damage);
             transform.parent.gameObject.GetComponent<PlayerHealth>().TakeDamage(other.transform.parent.GetComponent<EnemyStats>().activeAttack.damage);
         }

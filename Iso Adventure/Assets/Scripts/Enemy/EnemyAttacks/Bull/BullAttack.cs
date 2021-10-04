c322064e3be1556529e5cae4948346c2e5299f6b
Arilyn <arilyn.hess@gmail.com>
2021-10-04 01:57:00 -0600

    Enemy attacks

---
+++ Iso Adventure/Assets/Scripts/Enemy/EnemyAttacks/Bull/BullAttack.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BullAttack : MonoBehaviour, IEnemyAttack
+{
+
+    EnemyStats stats;
+
+    string attackName;
+    string animationName;
+
+    int damage;
+    float range;
+    float boxStart;
+    float boxEnd;
+
+    int nextAttack;
+
+    void Start()
+    {
+        stats = GetComponent<EnemyStats>();
+    }
+
+    public void InitializeAttack(string name, string anim, int dmg, float rng, float start, float end, int next)
+    {
+        attackName = name;
+        animationName = anim;
+        damage = dmg;
+        range = rng;
+        boxStart = start;
+        boxEnd = end;
+        nextAttack = next;
+    }
+
+    public void ExecuteAttack()
+    {
+        Debug.Log("Initializing attack: " + attackName + " " + animationName + " " + damage + " " + boxStart + " " + boxEnd + " " + nextAttack + " ");
+
+
+    }
+
+}

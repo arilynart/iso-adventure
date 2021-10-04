1f9bc382b3531488dac3e731939dbbbc171a1c6a
Arilyn <arilyn.hess@gmail.com>
2021-10-04 00:19:31 -0600

    Pause Menu works with controller

--- Iso Adventure/Assets/Prefabs/PauseMenu.cs
+++ Iso Adventure/Assets/Prefabs/PauseMenu.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseCanvas;
+
+    public GameObject pauseFirstButton;
 
     public static PauseMenu INSTANCE;
 
@@ -26,12 +30,23 @@ public class PauseMenu : MonoBehaviour
         {
             INSTANCE.pauseCanvas.SetActive(true);
             Time.timeScale = 0;
+
+            EventSystem es = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+            //clear selected object
+            es.SetSelectedGameObject(null);
+
+            es.SetSelectedGameObject(INSTANCE.pauseFirstButton);
         }
         else
         {
             INSTANCE.pauseCanvas.SetActive(false);
             Time.timeScale = 1;
         }
+    }
+
+    public static void QUIT()
+    {
+        Application.Quit();
     }
 
 }

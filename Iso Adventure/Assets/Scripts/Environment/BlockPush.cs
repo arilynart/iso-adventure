41dc481898335b0c0a41f0da54980d97f7f1d67b
Arilyn <arilyn.hess@gmail.com>
2021-10-09 23:25:52 -0600

    block buttons and block consuming works

---
+++ Iso Adventure/Assets/Scripts/Environment/BlockPush.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Arilyn.DeveloperConsole.Behavior;
+
+public class BlockPush : MonoBehaviour
+{
+    public Vector3 trajectory;
+    public float movespeed;
+    bool moving;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        trajectory = Vector3.zero;
+    }
+
+    void Update()
+    {
+        if (moving)
+        {
+            transform.position += trajectory * movespeed * Time.deltaTime;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        moving = false;
+        trajectory = Vector3.zero;
+    }
+
+    public void Slide(Vector3 direction)
+    {
+        trajectory = LockDirection(direction);
+        if (Physics.Raycast(transform.position, trajectory, 0.6f, DeveloperConsoleBehavior.PLAYER.ground)) return;
+
+        moving = true;
+    }
+
+    Vector3 LockDirection(Vector3 dir)
+    {
+        float dirX = 0;
+        float dirZ = 0;
+        if (Mathf.Abs(dir.x) > Mathf.Abs(dir.z)) {
+            if (dir.x > 0)
+            {
+                dirX = 1;
+                dirZ = 0;
+            }
+            else
+            {
+                dirX = -1;
+                dirZ = 0;
+            }
+        }
+        else if(Mathf.Abs(dir.z) > Mathf.Abs(dir.x))
+        {
+            if (dir.z > 0)
+            {
+                dirZ = 1;
+                dirX = 0;
+            }
+            else
+            {
+                dirZ = -1;
+                dirX = 0;
+            }
+        }
+        return new Vector3(dirX, 0, dirZ);
+    }
+}
---
+++ Iso Adventure/Assets/Scripts/Environment/Torch/BlockGateController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Arilyn.DeveloperConsole.Behavior;
+
+public class BlockGateController : MonoBehaviour, ITorchController
+{
+    [SerializeField]
+    private int requiredTorches;
+    public int RequiredTorches
+    {
+        get { return requiredTorches;  }
+        set { requiredTorches = value;  }
+    }
+    private int currentTorches;
+    public int CurrentTorches
+    {
+        get { return currentTorches; }
+        set { currentTorches = value; }
+    }
+
+    [SerializeField]
+    private GameObject gate;
+
+    void Start()
+    {
+        CurrentTorches = 0;
+    }
+
+    public void LightTorch()
+    {
+        CurrentTorches = 0;
+        Debug.Log("torch lit");
+        foreach (Transform child in transform)
+        {
+            if (child.GetComponent<BlockButton>().activated)
+            {
+                CurrentTorches++;
+            }
+        }
+        if (CurrentTorches >= RequiredTorches)
+        {
+            FullTorch();
+        }
+
+    }
+
+    public void FullTorch()
+    {
+        Camera.main.GetComponent<Cutscene>().CutsceneStart(gate.transform, 3f);
+        StartCoroutine(GateDelay());
+    }
+
+    IEnumerator GateDelay()
+    {
+        yield return new WaitForSeconds(1);
+        gate.GetComponent<GateController>().Open();
+    }
+
+}
